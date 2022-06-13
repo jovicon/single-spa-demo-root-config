5afca53154b61607d92175e05ca5be10ef3c11cc
Jose Contreras <jocontrera16@gmail.com>
2022-06-12 22:24:09 -0400

    added: express server config

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path = require("path");
 const PORT = process.env.PORT || 5000;
 
 express()
-  .use(express.static(path.join(__dirname, "dist")))
+  .use(express.static(path.join(__dirname, "dist/single-spa-demo")))
   .get("*", (req, res) => {
-    res.sendFile("index.html", { root: "dist" });
+    res.sendFile(__dirname + "/dist/single-spa-demo/index.html");
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
